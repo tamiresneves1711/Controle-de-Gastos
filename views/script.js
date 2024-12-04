0ac0949a5ec2e4bae1f1b2380774b37e8446975f
tamiresneves1711 <tamiresneves1711@gmail.com>
2024-12-04 15:24:09 -0300

    Add files via upload

--- views/script.js
+++ views/script.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://localhost:3000/api/expenses';
+const API_URL = 'https://controle-de-gastos-hnqr.onrender.com/api/expenses';
 
 document.getElementById('expenseForm').addEventListener('submit', async (e) => {
     e.preventDefault();
